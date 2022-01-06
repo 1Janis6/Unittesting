710e3651b3081902bcacc449fd1ad47ec0b39a78
1Janis6 <janis.ringli@gmail.com>
2022-01-06 16:45:11 +0100

    added testcase summeFalse

--- 02_01_SaveCalculatorMVN_Vorgabe/02_01_SaveCalculatorMVN_Vorgabe/src/test/java/ch/bbw/pr/savecalculator/SaveCalculatorTest.java
+++ 02_01_SaveCalculatorMVN_Vorgabe/02_01_SaveCalculatorMVN_Vorgabe/src/test/java/ch/bbw/pr/savecalculator/SaveCalculatorTest.java
@@ -8,8 +8,17 @@ import static org.junit.Assert.assertTrue;
 public class SaveCalculatorTest {
 
     @Test
-    public void shouldAnswerTrue() {
+    public void shouldAnswerTrue(){
         assertTrue(true);
+
+    }
+
+    @Test
+    public void summeFalse() {
+        SaveCalculator testee = new SaveCalculator();
+        int summand1 = 20;
+        int summand2 = 30;
+        assertFalse(testee.summe(summand1, summand2) == 40);
 
     }
 }
