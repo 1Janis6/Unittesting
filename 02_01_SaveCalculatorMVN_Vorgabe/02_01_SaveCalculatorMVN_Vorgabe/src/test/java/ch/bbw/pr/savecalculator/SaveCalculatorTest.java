129835530d4c92422aa06d2ad7409bd18a12e29f
1Janis6 <janis.ringli@gmail.com>
2022-01-06 16:43:07 +0100

    Added Test to see if testing works

--- 02_01_SaveCalculatorMVN_Vorgabe/02_01_SaveCalculatorMVN_Vorgabe/src/test/java/ch/bbw/pr/savecalculator/SaveCalculatorTest.java
+++ 02_01_SaveCalculatorMVN_Vorgabe/02_01_SaveCalculatorMVN_Vorgabe/src/test/java/ch/bbw/pr/savecalculator/SaveCalculatorTest.java
@@ -8,40 +8,8 @@ import static org.junit.Assert.assertTrue;
 public class SaveCalculatorTest {
 
     @Test
-    public void shouldAnswerTrue(){
+    public void shouldAnswerTrue() {
         assertTrue(true);
 
-    }
-
-    @Test
-    public void summeFalse() {
-        SaveCalculator testee = new SaveCalculator();
-        int summand1 = 20;
-        int summand2 = 30;
-        assertFalse(testee.summe(summand1, summand2) == 40);
-
-    }
-    @Test
-    public void summeEqualsZero() {
-        SaveCalculator testee = new SaveCalculator();
-        int summand1 = 0;
-        int summand2 = 0;
-        assertTrue(testee.summe(summand1, summand2) == 0);
-
-    }
-
-    @Test
-    public void summeWithNegativeNumbers() {
-        SaveCalculator testee = new SaveCalculator();
-        int summand1 = 0;
-        int summand2 = -1;
-        assertTrue(testee.summe(summand1, summand2) == -1);
-    }
-    @Test
-    public void summeWithMaxValue() {
-        SaveCalculator testee = new SaveCalculator();
-        int summand1 = Integer.MAX_VALUE;
-        int summand2 = 1;
-        assertFalse(testee.summe(summand1, summand2) == 1);
     }
 }
